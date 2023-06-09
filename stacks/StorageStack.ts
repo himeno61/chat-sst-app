28852d5384a539b1365eacaf91069130f44bf1fb
himeno61 <binkiewicz.luke@gmail.com>
2023-06-09 12:18:12 +0200

    Add StorageStack.ts and handling functions for websocket

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,32 @@
+import { StackContext, Table } from "sst/constructs";
+import { RemovalPolicy } from "aws-cdk-lib";
+
+export function StorageStack({ stack }: StackContext) {
+    // Create the table
+    const database = new Table(stack, "StorageTableV2", {
+        fields: {
+            pk: "string",
+            sk: "string",
+            gsi1pk: "string",
+            gsi1sk: "string",
+        },
+        primaryIndex: { partitionKey: "pk", sortKey: "sk" },
+        globalIndexes: {
+            "gsi1pk-gsi1sk-index": {
+                partitionKey: "gsi1pk",
+                sortKey: "gsi1sk",
+            },
+        },
+        cdk: {
+            table: {
+                removalPolicy: stack.stage === "prod" ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
+            },
+        },
+    });
+
+    stack.addOutputs({
+        DatabaseName: database.tableName,
+    });
+
+    return { database };
+}
---
+++ stacks/WebsocketStack.ts
@@ -0,0 +1,25 @@
+import {StackContext, use, WebSocketApi} from "sst/constructs";
+import {StorageStack} from "./StorageStack";
+
+export function WebsocketStack({ stack }: StackContext) {
+    const { database } = use(StorageStack);
+
+    // Create the WebSocket API
+    const webSocketApi = new WebSocketApi(stack, "WebsocketApi", {
+        defaults: {
+            function: {
+                bind: [database],
+            },
+        },
+        routes: {
+            $connect: "packages/functions/src/connect.main",
+            $disconnect: "packages/functions/src/disconnect.main",
+            sendmessage: "packages/functions/src/sendMessage.main",
+        },
+    });
+
+    // Show the API endpoint in the output
+    stack.addOutputs({
+        WebsocketApiEndpoint: webSocketApi.url,
+    });
+}
