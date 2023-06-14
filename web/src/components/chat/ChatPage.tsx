776a6bde2ee08f655c904aa55ebaa7f7cf9553e0
himeno61 <binkiewicz.luke@gmail.com>
2023-06-14 10:56:55 +0200

    Debugging react hook

--- web/src/components/chat/ChatPage.tsx
+++ web/src/components/chat/ChatPage.tsx
@@ -12,48 +12,32 @@ const ChatPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const username = location.state.userName;
-    const ws = useRef<WebSocket|null>(null);
+    const ws = useRef<WebSocket | null>(null);
     const [messages, setMessages] = useState<WebsocketMessage[]>([]);
     const [isConnected, setIsConnected] = useState(false);
     const lastElementRef = useRef<HTMLDivElement>(null);
 
 
+    function handleNewMessage(websocketMessage: WebsocketMessage) {
+        console.log(`Received message: ${JSON.stringify(websocketMessage)}`);
+        const findResult = messages.find(wsMessage => wsMessage.id === websocketMessage.id);
+        const ids = messages.map(m => m.id);
+        console.log(`ids of messages: ${ids}`)
+        if (findResult) {
+            console.log(`Item with id: ${websocketMessage.id} already in the table`);
+            return;
+        } else {
+            console.log("else");
+            setMessages([...messages, websocketMessage]);
+        }
+    };
+
     useEffect(() => {
-        const socket = new WebSocket(wssApiUrl);
         if (!location.state || username === "") {
             navigate("/");
         }
 
-
-        socket.onopen = () => {
-            console.log("opened");
-            setIsConnected(true);
-        };
-
-        socket.onmessage = (message) => {
-            if (message.data) {
-                const websocketMessage = JSON.parse(message.data) as WebsocketMessage;
-                console.log(`Received message: ${message.data}`);
-                const findResult = messages.find(wsMessage => wsMessage.id === websocketMessage.id);
-                const ids = messages.map(m => m.id);
-                console.log(`ids of messages: ${ids}`)
-                if (findResult) {
-                    console.log(`Item with id: ${websocketMessage.id} already in the table`);
-                    return;
-                } else {
-                    console.log("else");
-                    setMessages(messages.concat([websocketMessage]));
-                }
-            }
-        }
-        socket.onerror = () => {
-            console.log("error");
-            alert("Error with connection occured");
-            socket.close();
-            navigate("/")
-        }
-
-        ws.current = socket;
+        ws.current = new WebSocket(wssApiUrl);
 
         fetch(messagesApiUrl)
             .then(response => response.text())
@@ -70,13 +54,42 @@ const ChatPage = () => {
                     const diff = parsedMessages.filter(parsedMessage => !messages.some(websocketMessage => {
                         return parsedMessage.id === websocketMessage.id
                     }))
-
                     setMessages(parsedMessages.concat(diff));
                 }
             })
             .catch(err => console.log(err))
 
+        const wsCurrent = ws.current;
+
+        return () => {
+            if (wsCurrent?.readyState === 1)
+                wsCurrent?.close();
+        };
     }, []);
+
+    useEffect(()=>{
+        if(!ws.current) return;
+
+        ws.current.onopen = () => {
+            console.log("opened");
+            setIsConnected(true);
+        };
+
+        ws.current.onclose = () => {
+            console.log("ws closed");
+        }
+
+        ws.current.onmessage = (message) => {
+            if (message.data) {
+                const websocketMessage = JSON.parse(message.data) as WebsocketMessage;
+                handleNewMessage(websocketMessage);
+            }
+        }
+        ws.current.onerror = (e) => {
+            console.log("error");
+            console.log(e);
+        }
+    },[ws,messages]);
 
     function disconnect() {
         console.log("disconnect button")
